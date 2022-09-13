c4dfc7ccad1b2c73f928f06e053ce728fd16fbfe
jaengustavof <jaengustavof@gmail.com>
2022-09-13 18:14:09 +0200

    chore/ajuste index.js

--- configs/db.js
+++ configs/db.js
@@ -1,3 +1,4 @@
 const slonik = require("slonik");
+// en las versiones nuevas hay que poner await porque es un apromesa// Da el error fn is not a function
 
 module.exports = slonik.createPool(process.env.DB_URL);
