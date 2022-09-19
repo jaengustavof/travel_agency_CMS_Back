85e15e7b80019d0addc32a1b409c59f4d9ed9c41
jaengustavof <jaengustavof@gmail.com>
2022-09-19 21:12:53 +0200

    cors

--- configs/cors.js
+++ configs/cors.js
@@ -1,4 +1,4 @@
-const whitelist = ["https://proyecto-final-front-qcwr9ft8t-jaengustavof.vercel.app/", "http://localhost:3002", "127.0.0.1", "https://localhost:3002"];
+const whitelist = ["https://proyecto-final-front-qcwr9ft8t-jaengustavof.vercel.app", "http://localhost:3002", "127.0.0.1", "https://localhost:3002"];
 
 var corsOptions = {
     origin: function (origin, callback) {
