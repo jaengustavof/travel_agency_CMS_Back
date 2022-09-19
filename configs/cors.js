0405b0770577f00168267aea1880cf5339796c87
jaengustavof <jaengustavof@gmail.com>
2022-09-19 21:07:48 +0200

    cors

--- configs/cors.js
+++ configs/cors.js
@@ -1,13 +1,16 @@
 const whitelist = ["https://proyecto-final-front-qcwr9ft8t-jaengustavof.vercel.app/", "http://localhost:3002", "127.0.0.1", "https://localhost:3002"];
 
+var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    credentials: true,
+  }
 module.exports = {
-  origin: (origin, callback) => {
-    console.log('origin ', origin)
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error({message: "Cors Error"}));
-    }
-  },
-  credentials: true,
+    corsOptions
+  
 };
