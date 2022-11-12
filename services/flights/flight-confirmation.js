6515d010519e84b0f15f4f7412c30ac606dbb6f3
jaengustavof <jaengustavof@gmail.com>
2022-11-12 14:15:08 +0100

    flight confirmation

--- services/flights/flight-confirmation.js
+++ services/flights/flight-confirmation.js
@@ -1,24 +1,21 @@
 const Amadeus = require('amadeus');
 const amadeus = new Amadeus({
-    clientId: process.env.AMADEUS_CLIENTID,
-    clientSecret: process.env.AMADEUS_CLIENTSECRET,  
+    clientId: 'hLTNNmBLAAry5gQ2xLKAZxEBJ9QN2Ux5',
+    clientSecret: 'kMk70AxbxzFUbcVb',  
 });
 
 module.exports =() => async(req, res, next) =>{
 
     const flight = req.body
-    console.log(flight)
-    res.status(200).json({
-        success: true,
-        data: flight
-    })
+    
     // Confirm availability and price
-    /*
     amadeus.shopping.flightOffers.pricing.post(
         JSON.stringify({
-            'data': {
-                'type': 'flight-offers-pricing',
-                'flightOffers': [flight],
+            "data": {
+                "type": "flight-offers-pricing",
+                "flightOffers": [
+                    flight
+                ]
             }
         })
     ).then(function (response) {
@@ -26,6 +23,5 @@ module.exports =() => async(req, res, next) =>{
         }).catch(function (response) {
             res.send(response)
         })
-
-    */
+   
 }
